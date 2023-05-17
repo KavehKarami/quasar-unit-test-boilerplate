7b6c5de958261564ac5e714025ddfda02996382c
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-05-17 23:08:54 +0330

    add counter and counter increment

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+  <q-page class="flex flex-center column jusify-center">
+    <p>
+      counter : <span data-testid="counter">{{ store.counter }}</span>
+    </p>
+    <q-btn
+      color="primary"
+      data-testid="button"
+      type="button"
+      @click="store.increment"
     >
+      Increment!
+    </q-btn>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
+</script>
+
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+
+const store = useCounterStore();
 </script>
