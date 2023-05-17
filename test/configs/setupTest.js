00181023a62075b171ce62adf315e1f41591e18f
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-05-17 23:39:54 +0330

    reset add stores after each test & global plugins

--- test/configs/setupTest.js
+++ test/configs/setupTest.js
@@ -1,5 +1,16 @@
 import "@testing-library/jest-dom";
 import { installQuasarPlugin } from "@quasar/quasar-app-extension-testing-unit-jest";
+import { getActivePinia } from "pinia";
+import Router from "@/router";
+import Store from "@/stores";
 
 installQuasarPlugin();
 window.scrollTo = jest.fn();
+
+global.router = Router();
+global.store = Store();
+
+// NOTE: reset all stores after each test
+afterEach(() => {
+  getActivePinia()._s.forEach((store) => store.$reset());
+});
